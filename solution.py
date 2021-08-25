18280df44477e44c83bc624953f54442568d5a2e
DrAa <DrAa@DrAa.com>
2021-08-25 21:01:10 +0800

    FINE

--- solution.py
+++ solution.py
@@ -6,16 +6,13 @@ from luma.core.virtual import viewport
 from luma.led_matrix.device import max7219
 import time
 
-serial = spi(port=0, device=0, gpio=noop(), block_orientation=90)
+serial = spi(port=0, device=0, gpio=noop(), block_orientation=0)
 device = max7219(serial,width=32,block_orientation=-90)
 virtual = viewport(device, width=300, height=100)
 t = time.strftime(r"%Y-%m-%d %H:%M:%S",time.gmtime())
 
 while(True):   
-
     with canvas(virtual) as draw:
-        #print(type(draw))
-        #draw.rectangle(device.bounding_box, outline="white", fill="black")
         draw.text((3, 3), f"current time:{t}", fill="white", stroke_width=2)
 
     for offset in range(180):
--- start_from_here.py
+++ start_from_here.py
@@ -8,14 +8,13 @@ import time
 
 #Please fill the angle brackets part
 
-serial = spi(port=0, device=0, gpio=noop(), block_orientation=90)
+serial = spi(port=0, device=0, gpio=noop(), block_orientation=<>)
 device = max7219(serial,width=<>,block_orientation=<>)
 virtual = viewport(device, width=200, height=100)
-t = time.strftime(r"%Y%m%d",time.gmtime())
+t = time.strftime(<>)
 
 while(True):
     with canvas(virtual) as draw:
-        #draw.rectangle(device.bounding_box, outline="white", fill="black")
         draw.text(<>, f"current time:{t}", fill="white")
 
     for offset in range(<>):
